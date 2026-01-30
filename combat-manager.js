84f6a4722ba389192d9fada77aa82bd5a1730e3e
Imre Baas <imspons@gmail.com>
2026-01-30 09:28:13 +0100

    move methods to seperate file

--- combat-manager.js
+++ combat-manager.js
@@ -1,4 +1,5 @@
 import { Constants } from "./constants.js";
+import { TrackingHelper } from "./tracking-helper.js";
 
 export class CombatManager {
   static onDeleteCombat(combat, _, __) {
@@ -28,7 +29,7 @@ export class CombatManager {
 
         // Remove corresponding active effects
         removedNotes.forEach(async note => {
-          await game.combatManager._removeAdditionalNoteEffects(token, note);
+          await TrackingHelper.removeAdditionalNoteEffects(token, note);
         });
 
         // Create chat message with removed notes
@@ -67,7 +68,7 @@ export class CombatManager {
 
         // Remove corresponding active effects
         removedNotes.forEach(async note => {
-          await game.combatManager._removeAdditionalNoteEffects(token, note);
+          await TrackingHelper.removeAdditionalNoteEffects(token, note);
         });
 
         // Create chat message with removed notes
@@ -121,7 +122,7 @@ export class CombatManager {
       if (success) {
         const updatedNotesArray = notesArray.filter(n => n !== note);
         token.setFlag("too-many-modifiers", "notes", updatedNotesArray);
-        await game.combatManager._removeAdditionalNoteEffects(token, note);
+        await TrackingHelper.removeAdditionalNoteEffects(token, note);
         game.combatManager._createRemovedNotesMessage(token.name, [note]);
       }
     });
@@ -218,85 +219,5 @@ export class CombatManager {
       content: content,
       type: CONST.CHAT_MESSAGE_TYPES.OTHER
     });
-  }
-
-  //TODO: Move these 4 methods to a seperate manager class. Can be statics.
-
-  _removeConditionEffect(token, note) {
-    if (!token?.actor) return;
-
-    const conditionName = note.text;
-    const actor = token.actor;
-    const effect = actor.effects.find(e => e.name === conditionName);
-
-    if (effect) {
-      effect.delete();
-    }
-  }
-
-  async _removeModifierBonus(token, note) {
-    if (!token?.actor) return;
-
-    const actor = token.actor;
-    const modifierType = note.modifierType;
-    const bonusName = `too-many-modifiers: ${note.text}`;
-
-    if (!modifierType) return;
-
-    switch (modifierType) {
-      case 'AC':
-        const acBonus = actor.system.defences.ac.bonus || [];
-        const updatedAcBonus = acBonus.filter(b => b.name !== bonusName);
-
-        console.log(acBonus);
-        console.log(updatedAcBonus);
-        await actor.update({ 'system.defences.ac.bonus': updatedAcBonus });
-        break;
-      case 'Speed':
-        const speedBonus = actor.system.movement.base.bonus || [];
-        const updatedSpeedBonus = speedBonus.filter(b => b.name !== bonusName);
-        await actor.update({ 'system.movement.base.bonus': updatedSpeedBonus });
-        break;
-      case 'Damage':
-        const damageBonus = actor.system.modifiers.damage.bonus || [];
-        const updatedDamageBonus = damageBonus.filter(b => b.name !== bonusName);
-        await actor.update({ 'system.modifiers.damage.bonus': updatedDamageBonus });
-        break;
-      case 'Saving Throws':
-        const saveBonus = actor.system.details.saves.bonus || [];
-        const updatedSaveBonus = saveBonus.filter(b => b.name !== bonusName);
-        await actor.update({ 'system.details.saves.bonus': updatedSaveBonus });
-        break;
-      case 'Attacks':
-        const attackBonus = actor.system.modifiers.attack.bonus || [];
-        const updatedAttackBonus = attackBonus.filter(b => b.name !== bonusName);
-        await actor.update({ 'system.modifiers.attack.bonus': updatedAttackBonus });
-        break;
-      default:
-        ui.notifications.warn(`Modifier type "${modifierType}" is not supported yet.`);
-        break;
-    }
-  }
-
-  async _removeResistanceBonus(token, note) {
-    if (!token?.actor) return;
-
-    const actor = token.actor;
-    const resistanceType = note.resistanceType;
-    const bonusName = `too-many-modifiers: ${note.text}`;
-
-    if (!resistanceType) return;
-
-    const resistancePath = `system.resistances.${resistanceType}.bonus`;
-    const resistanceBonus = getProperty(actor, resistancePath) || [];
-    const updatedResistanceBonus = resistanceBonus.filter(b => b.name !== bonusName);
-    await actor.update({ [resistancePath]: updatedResistanceBonus });
-  }
-
-  /// Cleans up all effects related to a note to prevent any weird leftovers.
-  async _removeAdditionalNoteEffects(token, note) {
-    game.combatManager._removeConditionEffect(token, note);
-    await game.combatManager._removeModifierBonus(token, note);
-    await game.combatManager._removeResistanceBonus(token, note);
   }
 }
